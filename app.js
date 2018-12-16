a860602a90f806d8e7e2c0f5586a969d4e657508
Aleksey Belchenkov <u608110@gmail.com>
2018-12-16 18:00:19 +0300

    Arrow Functions

--- app.js
+++ app.js
@@ -5,24 +5,16 @@ document.getElementById('button3').addEventListener('click', getExternal);
 // Get local text file data
 function getText () {
     fetch('test.txt')
-        .then(function (res) {
-            return res.text();
-        })
-        .then(function (data) {
-            document.getElementById('output').innerHTML = data;
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
+        .then(res => res.text())
+        .then(data => document.getElementById('output').innerHTML = data)
+        .catch(err => console.error(err));
 }
 
 // Get local json data
 function getJson () {
     fetch('posts.json')
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
+        .then(res => res.json())
+        .then(data => {
             let output = '';
 
             data.forEach(function (post) {
@@ -30,18 +22,14 @@ function getJson () {
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function (err) {
-            console.error(err);
-        });
+        .catch(err => console.error(err));
 }
 
 // Get local external API
 function getExternal () {
     fetch('https://api.github.com/users')
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
+        .then(res => res.json())
+        .then(data => {
             let output = '';
 
             data.forEach(function (user) {
@@ -49,7 +37,5 @@ function getExternal () {
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function (err) {
-            console.error(err);
-        });
+        .catch(err => console.error(err));
 }
