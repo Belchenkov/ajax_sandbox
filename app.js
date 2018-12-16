6d9520e09d8b7a479aebc3d85aad988907c7f5d2
Aleksey Belchenkov <u608110@gmail.com>
2018-12-16 18:50:03 +0300

    Async/Await

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getJson);
-document.getElementById('button3').addEventListener('click', getExternal);
+/*
+async function myFunc() {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('Hello'), 2000)
+    });
 
-// Get local text file data
-function getText () {
-    fetch('test.txt')
-        .then(res => res.text())
-        .then(data => document.getElementById('output').innerHTML = data)
-        .catch(err => console.error(err));
+    const error = false;
+
+    if (!error) {
+        return await promise; // Wait until promise is resolved
+    } else {
+        await Promise.reject(new Error('Something went wrong!!!'))
+    }
 }
 
-// Get local json data
-function getJson () {
-    fetch('posts.json')
-        .then(res => res.json())
-        .then(data => {
-            let output = '';
+myFunc()
+    .then(res => console.log(res))
+    .catch(err => console.error(err));*/
 
-            data.forEach(function (post) {
-               output += `<li>${post.title}</li>`;
-            });
-            document.getElementById('output').innerHTML = output;
-        })
-        .catch(err => console.error(err));
+async function getUsers () {
+    // await response of the fetch call
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    // Only proceed once its resolved
+    const data = await res.json();
+
+    // only proceed once second promise is resolved
+    return data;
 }
 
-// Get local external API
-function getExternal () {
-    fetch('https://api.github.com/users')
-        .then(res => res.json())
-        .then(data => {
-            let output = '';
-
-            data.forEach(function (user) {
-               output += `<li>${user.login}</li>`;
-            });
-            document.getElementById('output').innerHTML = output;
-        })
-        .catch(err => console.error(err));
-}
+getUsers().then(users => console.log(users));
